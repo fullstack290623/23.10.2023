b8c275530bc7c083b029f834a8a8b61b9027a897
micserv010821 <fullstack290623@gmail.com>
2023-10-23 21:27:59 +0300

    update

--- node1/app.js
+++ node1/app.js
@@ -1,16 +1,16 @@
 console.log('Hello node!');
 let x = 1
 let y = 2
-console.log(`${x} + ${y} = ${x+y}`);
+console.log(`${x} + ${y} = ${x + y}`);
 
 function multiply(a, b) {
-    console.log(`${a} * ${b} = ${a*b}`);
+    console.log(`${a} * ${b} = ${a * b}`);
 }
 
 multiply(3, 5);
 
 const os = require('os') // bring 'os' library to my program
-console.log(`Free memory = ${os.freemem / 1024 / 1024 } MB`)
+console.log(`Free memory = ${os.freemem / 1024 / 1024} MB`)
 console.log(`Total memory = ${os.totalmem() / 1024 / 1024 / 1024} GB`)
 
 console.log(`Operating System: ${os.platform()}`);
@@ -21,10 +21,37 @@ const cpus = os.cpus();
 console.log(`Number of CPU Cores: ${cpus.length}`);
 console.log('CPU Information:');
 cpus.forEach((cpu, index) => {
-  console.log(`  Core ${index + 1}: ${cpu.model}`);
+    console.log(`  Core ${index + 1}: ${cpu.model}`);
 });
 
 console.log(`User Info:`);
 console.log(`Username: ${os.userInfo().username}`);
 console.log(`Home Directory: ${os.userInfo().homedir}`);
+
+const fs = require('fs').promises;
+const path = require('path');
+
+async function readFilesInFolder(folderPath) {
+    const files = await fs.readdir(folderPath);
+    for (const file of files) {
+        console.log(`File: ${file}`);
+        console.log('------------------');
+    }
+
+    const readline = require('readline');
+
+    // demo code how to prompt 
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    
+    rl.question('What is your name? ', (name) => {
+      console.log(`Hello, ${name}!`);
+      rl.close();
+    });    
+}
+
+const folderPath = '.'; // Replace with the path to your folder
+readFilesInFolder(folderPath);
 
